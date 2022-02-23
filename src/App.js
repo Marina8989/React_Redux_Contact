023bd4e182d265a2207212ae7706d47249c71e8e
Marina8989 <marina.mill209@gmail.com>
2022-02-23 15:32:12 -0800

    redux

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { incrementCounter } from './store';
+import {connect } from 'react-redux';
+import { incrementCounter } from  './store';
 
 const App = (props) => {
     return (
         <>
-         <h2>Redux Tutorial</h2>
-         <h4 onClick={props.increment}>{props.counter}</h4>
+        <h2>Redux...</h2>
+        <h4 onClick={props.increment}>{props.counter}</h4>
         </>
     )
 }
-
 const mapStateToProps = (state) => ({
     counter: state.counter
 })
+
 const mapDispatchToProps = {
     increment: incrementCounter
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,19 @@
 import {combineReducers, createStore} from 'redux';
 
-const initialState = 1
+const initialState= 1;
 
-function counterReducer(state=initialState, action){
-    console.log(action)
-   if(action.type === 'INCREMENT_COUNTER') {
-    return state + 1;
-   }
+function counterReducer(state = initialState, action){
+    if(action.type === "INCREMENT_COUNTER") {
+        return state + 1
+    }
     return state;
 }
 
 export const incrementCounter = () => {
-    console.log('clicking')
     return {
-        type: 'INCREMENT_COUNTER'
+        type: "INCREMENT_COUNTER"
     }
 }
-
 
 const reducers = combineReducers({
     counter: counterReducer
