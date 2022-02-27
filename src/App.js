f34f464dc92ba16e69ff6db35f7197703aa4dae5
Marina8989 <marina.mill209@gmail.com>
2022-02-26 19:04:27 -0800

    redux

--- src/App.js
+++ src/App.js
@@ -1,39 +1,76 @@
-import React, {useState} from 'react';
-import {connect} from 'react-redux';
-import { addTodo, removeTodo } from './store/todos/actions';
+import React from 'react';
+import './index.css';
 
-const App = (props) => {
-    const [value, setValue] = useState('')
 
-    const handleSubmit = (e) => {
-       e.preventDefault();
-       props.addTodo(value);
-       setValue('');
-    }
+const Item = (props) => {
+    console.log(props)
     return (
-        <>
-          <form onSubmit={handleSubmit}>
-              <input value={value} onChange={(e) => setValue(e.target.value)}/>
-          </form>
-          <div style={{display: 'flex'}}>
-          <div>
-          {props.todos.map(todo => <li onClick={() => props.removeTodo(todo)} key={todo}>{todo}</li>)}
-          </div>
-          <div>
-         {props.deletedTodos.map(el => <li key={el}>{el}</li>)}
-         </div>
-         </div>
-        </>
+      <>  
+        <li className={props.item.complete ? 'completed-item' : ''}>{props.item.value}</li>
+        <button onClick={() => props.handleToggle(props.item)}>toggle</button>
+      </>  
     )
 }
-const mapStateToProps = (state) => ({
-  todos: state.todos.data,
-  deletedTodos: state.deletedTodos.data
-})
 
-const mapDispatchToProps = {
-    addTodo,
-    removeTodo
+const List = (props) => {
+    return (
+      <>  
+        {props.list.map(el => <Item item={el} key={el} handleToggle={props.handleToggle}/>)}
+      </>  
+    )
+}
+class Form extends React.Component {
+    state = {
+        inputValue: ''
+    }
+    handleChange = (e) => {
+       this.setState({inputValue: e.target.value})
+    }
+    handleSubmit = (e) => {
+       e.preventDefault();
+       this.setState({inputValue: ''})
+       this.props.handleSubmit(this.state.inputValue)
+    }
+    render(){
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input value={this.inputValue} onChange={this.handleChange}/>
+            </form>
+        )
+    }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+class App extends React.Component{
+   state = {
+        list: []
+    }
+    handleSubmit = (value) => {
+       const item = {
+           id: `${Math.random() - Math.random()}`,
+           value,
+           complete: false
+       }
+       this.setState({list: [...this.state.list, item]})
+    }
+
+    handleToggle = (val) => {
+       const newList = this.state.list.map(el => {
+           if(el.id === val.id) {
+             el.complete = !el.complete
+           }
+           return el
+       })
+       this.setState({list: newList})
+    }
+
+    render() {
+      return (
+        <>
+         <Form handleSubmit={this.handleSubmit} />
+         <List list={this.state.list} handleToggle={this.handleToggle}/>
+        </>
+      )
+    }
+}
+
+export default App;
--- src/store/deletedTodos/actions.js
+++
@@ -1,8 +0,0 @@
-import {ADD_REMOVED_TODO} from './index';
-
-export const addRemovedTodo = (user) => (dispatch, getState) => {
-    dispatch({
-        type: ADD_REMOVED_TODO,
-        payload: user
-    })
-}
